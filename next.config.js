cfa160e20e91c67dae0a50892610be223d9d14ed
juhamikael <contact.juhamusic@gmail.com>
2023-08-13 18:30:01 +0300

    Modify next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     mdxRs: true,
   },
 };
-const withMDX = require("@next/mdx")();
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+  
+});
 
 module.exports = withMDX(nextConfig);
