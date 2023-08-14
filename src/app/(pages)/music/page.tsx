f3548a8fb32f980ad4195139fedf966db24066dd
Juhamikael <contact.juhamusic@gmail.com>
2023-08-15 00:14:58 +0300

    Testing sanity on unfinished page

--- src/app/(pages)/music/page.tsx
+++ src/app/(pages)/music/page.tsx
@@ -1,16 +1,18 @@
+import Projects from "@/components/sanity/Projects";
 import { FC } from "react";
+import { cachedClient } from "@/../sanity/lib/client";
+import { projectsQuery } from "@/../sanity/lib/queries";
 
 interface IMusicProps {
   prop?: string;
   children?: React.ReactNode;
 }
 
-const MusicPage: FC<IMusicProps> = ({}) => {
+export default async function Music() {
+  const projects = await cachedClient(projectsQuery);
   return (
     <div>
-      <div>music</div>
+      <Projects projects={projects} />
     </div>
   );
-};
-
-export default MusicPage;
+}
