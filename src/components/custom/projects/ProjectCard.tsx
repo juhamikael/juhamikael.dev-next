8cc38bf6b991f01c6f2ca3a65ebab95c502d881f
Juhamikael <contact.juhamusic@gmail.com>
2023-08-15 00:17:13 +0300

    Sanity configuration for Project card

--- src/components/custom/projects/ProjectCard.tsx
+++ src/components/custom/projects/ProjectCard.tsx
@@ -9,90 +9,105 @@ import {
 } from "@/components/ui/card";
 import { IconsObject } from "@/lib/icons";
 import Link from "next/link";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import { AiTwotoneDownCircle } from "react-icons/ai";
 import StraightLine from "../../StraightLine";
-import { ProjectLanguagesObject } from "@/lib/projectLanguages";
-import { DownloadLinks, LiveLinks, GitHubLinks } from "@/lib/links";
-type LanguageKeys = keyof typeof ProjectLanguagesObject;
+import type { SanityDocument } from "@sanity/client";
+import _ from "lodash";
 
 interface ProjectCardProps {
-  prop?: string;
-  children?: React.ReactNode;
-  imgSrc?: StaticImageData;
   projectName: string;
-  languages: LanguageKeys[];
-  repoKey?: keyof typeof GitHubLinks;
-  downloadKey?: keyof typeof DownloadLinks;
-  liveKey?: keyof typeof LiveLinks;
+  project?: SanityDocument;
 }
+type LanguageTag = {
+  _type: string;
+  label: string;
+  _key: string;
+  value: string;
+};
 
-const ProjectCard: FC<ProjectCardProps> = ({
-  children,
-  projectName,
-  languages,
-  imgSrc,
-  repoKey = "main",
-  downloadKey = "main",
-  liveKey = "main",
-}) => {
+type BlockType = {
+  children: Array<{ text: string }>;
+};
+
+const getWidthAndHeight = (url: string) => {
+  if (!url) return { width: 300, height: 300 };
+  const regex = /-(\d+)x(\d+)\.jpg$/;
+  const match = url.match(regex);
+  if (match) {
+    const [_, width, height] = match;
+    return { width, height };
+  } else {
+    return { width: 300, height: 300 };
+  }
+};
+
+const ProjectCard: FC<ProjectCardProps> = ({ projectName, project }) => {
+  if (!project) return <div>Loading...</div>;
   const GitHubIcon = IconsObject["github"].icon;
   const DownloadIcon = IconsObject["download"].icon;
   const LiveIcon = IconsObject["live"].icon;
+  const languages: LanguageTag[] = project.languageTags;
 
-  const githubHref = GitHubLinks[repoKey];
-  const downloadHref = DownloadLinks[downloadKey] || "";
-  const liveHref = LiveLinks[liveKey] || "";
-
+  const { width, height } = getWidthAndHeight(project.imageUrl);
   return (
-    <Card className={`bg-transparent rounded-xl border border-card-foreground/20 `}>
+    <Card
+      className={`bg-transparent rounded-xl border border-card-foreground/20 `}
+    >
       <CardHeader className="flex-grow h-[5.5rem]">
         <CardTitle className="text-xl">{projectName}</CardTitle>
         <CardDescription className="text-xs flex gap-x-2">
           <AiTwotoneDownCircle className="text-primary" />
-          {languages
-            .map((langKey) => ProjectLanguagesObject[langKey])
-            .join(", ")}
+          {languages?.map((tag) => tag.label).join(", ") ?? "No Languages"}
         </CardDescription>
       </CardHeader>
       <CardContent className="w-fit">
         <StraightLine className="mb-2" />
         <div className="flex flex-col gap-x-10 mt-5 2xl:flex-row">
           <div className=" prose prose-p:text-sm prose-p:text-card-foreground">
-            {children}
+            {project.body.map((block: BlockType) => (
+              <p>{block.children[0].text}</p>
+            ))}
           </div>
-          {imgSrc && (
-            <Image className="mt-4 2xl:mt-0 w-72 h-72" src={imgSrc || ""} alt="" />
+          {project.imageUrl && (
+            <Image
+              className="mt-4 2xl:mt-0 w-72 h-72"
+              src={project.imageUrl || ""}
+              alt=""
+              width={width as number}
+              height={height as number}
+              priority={false}
+            />
           )}
         </div>
       </CardContent>
       <CardFooter className="flex justify-between items-center">
         <div className="flex items-center gap-x-4">
-          {repoKey !== "main" && (
+          {project.href.github && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
-              href={githubHref}
+              href={project.href.github}
             >
               <GitHubIcon />
               GitHub
             </Link>
           )}
-          {downloadKey !== "main" && (
+          {project.href.download && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
-              href={downloadHref}
+              href={project.href.download}
             >
               <DownloadIcon />
               Download
             </Link>
           )}
-          {liveKey !== "main" && (
+          {project.href.live && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
-              href={liveHref}
+              href={project.href.live}
             >
               <LiveIcon />
               Live
