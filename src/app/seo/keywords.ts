0076e6a260f8e0a9229888a3b77cb94ab083c904
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 20:00:47 +0300

    Create SEO options
    Will modify these later

---
+++ src/app/seo/keywords.ts
@@ -0,0 +1,10 @@
+export const keywords = {
+  contact: [
+    "juha",
+    "mikael",
+    "contact",
+    "juhamikael",
+    "juhamikael.info",
+    "juhamikael music",
+  ],
+};
---
+++ src/app/seo/title.ts
@@ -0,0 +1,8 @@
+export const title = {
+  home: "Juha Mikael / Home",
+  blog: "Juha Mikael / Blog",
+  projects: "Juha Mikael / Projects",
+  portfolio: "Juha Mikael / Portfolio",
+  skills: "Juha Mikael / Skills",
+  contact: "Juha Mikael / Contact",
+};
