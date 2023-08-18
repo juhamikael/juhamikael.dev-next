7f59dcc17cddbbcefa3c06c4f371696153c366f8
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 19:57:13 +0300

    Create ImageComponent.tsx

---
+++ src/components/ImageComponent.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import type { ImageComponent } from "@/app/types/pages/blog";
+import urlBuilder from "@sanity/image-url";
+import Image from "next/image";
+import { getImageDimensions } from "@sanity/asset-utils";
+import { client } from "@/../sanity/lib/client";
+import CustomDialog from "@/components/custom/CustomDialog";
+import StraightLine from "./StraightLine";
+
+const BlockImageComponent: FC<ImageComponent> = ({ value, isInline }) => {
+  const { width, height } = getImageDimensions(value);
+  return (
+    <>
+      <Image
+        className="my-3 border border-card-foreground/30 p-2 rounded-xl"
+        src={urlBuilder(client).image(value).fit("max").auto("format").url()}
+        width={width}
+        height={height}
+        alt={value.alt || " "}
+        loading="lazy"
+        style={{
+          display: isInline ? "inline-block" : "block",
+          //   aspectRatio: width / height,
+        }}
+      />
+
+      <CustomDialog>
+        <Image
+          className=""
+          src={urlBuilder(client).image(value).fit("max").auto("format").url()}
+          width={width}
+          height={height}
+          alt={value.alt || " "}
+          loading="lazy"
+          style={{
+            display: isInline ? "inline-block" : "block",
+          }}
+        />
+      </CustomDialog>
+      <StraightLine />
+    </>
+  );
+};
+
+export default BlockImageComponent;
