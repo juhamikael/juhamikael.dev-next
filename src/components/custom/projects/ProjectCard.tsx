8d65448510c8da7d1ea7f91334210247092d6903
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 20:01:49 +0300

    Update ProjectCard.tsx

--- src/components/custom/projects/ProjectCard.tsx
+++ src/components/custom/projects/ProjectCard.tsx
@@ -10,14 +10,23 @@ import {
 import { IconsObject } from "@/lib/icons";
 import Link from "next/link";
 import Image from "next/image";
-import { AiTwotoneDownCircle } from "react-icons/ai";
+import { BsFillCircleFill } from "react-icons/bs";
 import StraightLine from "../../StraightLine";
-import type { SanityDocument } from "@sanity/client";
-import _ from "lodash";
+import { SanityDocument } from "next-sanity";
+import { BsArrowRightCircleFill } from "react-icons/bs";
+import { getImageWidthAndHeight } from "@/lib/utils";
+import type { BlockType } from "@/app/types/projects";
+import { cn } from "@/lib/utils";
+import { proseClassName, prose } from "@/app/styles/prose";
 
-interface ProjectCardProps {
+interface ProjectCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string;
   projectName: string;
   project?: SanityDocument;
+  showImage?: boolean;
+  showBody?: boolean;
+  showReadMore?: boolean;
+  showHrefButtons?: boolean;
 }
 type LanguageTag = {
   _type: string;
@@ -26,64 +35,65 @@ type LanguageTag = {
   value: string;
 };
 
-type BlockType = {
-  children: Array<{ text: string }>;
-};
-
-const getWidthAndHeight = (url: string) => {
-  if (!url) return { width: 300, height: 300 };
-  const regex = /-(\d+)x(\d+)\.jpg$/;
-  const match = url.match(regex);
-  if (match) {
-    const [_, width, height] = match;
-    return { width, height };
-  } else {
-    return { width: 300, height: 300 };
-  }
-};
-
-const ProjectCard: FC<ProjectCardProps> = ({ projectName, project }) => {
+const ProjectCard: FC<ProjectCardProps> = ({
+  className,
+  projectName,
+  project,
+  showImage = true,
+  showBody = true,
+  showReadMore = false,
+  showHrefButtons = true,
+}) => {
   if (!project) return <div>Loading...</div>;
   const GitHubIcon = IconsObject["github"].icon;
   const DownloadIcon = IconsObject["download"].icon;
   const LiveIcon = IconsObject["live"].icon;
   const languages: LanguageTag[] = project.languageTags;
 
-  const { width, height } = getWidthAndHeight(project.imageUrl);
+  const { width, height } = getImageWidthAndHeight(project.imageUrl);
   return (
     <Card
-      className={`bg-transparent rounded-xl border border-card-foreground/20 `}
+      id={project.slug.current}
+      className={cn(
+        `bg-transparent rounded-xl border border-card-foreground/20 shadow-lg shadow-primary/20 drop-shadow-2xl`,
+        className ?? ""
+      )}
     >
       <CardHeader className="flex-grow h-[5.5rem]">
         <CardTitle className="text-xl">{projectName}</CardTitle>
+
         <CardDescription className="text-xs flex gap-x-2">
-          <AiTwotoneDownCircle className="text-primary" />
-          {languages?.map((tag) => tag.label).join(", ") ?? "No Languages"}
+          <BsFillCircleFill className="text-primary" />
+          <span className="truncate">
+            {languages?.map((tag) => tag.label).join(", ") ?? "No Languages"}
+          </span>
         </CardDescription>
       </CardHeader>
-      <CardContent className="w-fit">
-        <StraightLine className="mb-2" />
-        <div className="flex flex-col gap-x-10 mt-5 2xl:flex-row">
-          <div className=" prose prose-p:text-sm prose-p:text-card-foreground">
-            {project.body.map((block: BlockType) => (
-              <p>{block.children[0].text}</p>
-            ))}
+      <StraightLine className="mb-2" />
+      {showBody && (
+        <CardContent className="w-fit">
+          <div className="flex flex-col gap-x-10 mt-5 2xl:flex-row ">
+            <div className={cn("prose", prose.p)}>
+              {project.body.map((block: BlockType, index: number) => (
+                <p key={index}>{block.children[0].text}</p>
+              ))}
+            </div>
+            {showImage && project.imageUrl && (
+              <Image
+                className="mt-4 2xl:mt-0 w-72 h-72"
+                src={project.imageUrl || ""}
+                alt=""
+                width={width as number}
+                height={height as number}
+                priority={false}
+              />
+            )}
           </div>
-          {project.imageUrl && (
-            <Image
-              className="mt-4 2xl:mt-0 w-72 h-72"
-              src={project.imageUrl || ""}
-              alt=""
-              width={width as number}
-              height={height as number}
-              priority={false}
-            />
-          )}
-        </div>
-      </CardContent>
+        </CardContent>
+      )}
       <CardFooter className="flex justify-between items-center">
         <div className="flex items-center gap-x-4">
-          {project.href.github && (
+          {showHrefButtons && project.href.github && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
@@ -93,7 +103,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ projectName, project }) => {
               GitHub
             </Link>
           )}
-          {project.href.download && (
+          {showHrefButtons && project.href.download && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
@@ -103,7 +113,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ projectName, project }) => {
               Download
             </Link>
           )}
-          {project.href.live && (
+          {showHrefButtons && project.href.live && (
             <Link
               target="_blank"
               className="flex gap-x-2 items-center hover:text-primary transition-colors font-bold"
@@ -111,6 +121,15 @@ const ProjectCard: FC<ProjectCardProps> = ({ projectName, project }) => {
             >
               <LiveIcon />
               Live
+            </Link>
+          )}
+          {showReadMore && (
+            <Link
+              className="text-muted-foreground text-sm hover:text-nav-font-hover transition-colors"
+              href={`/projects#${project.slug.current}`}
+            >
+              Read More
+              <BsArrowRightCircleFill className="inline-block ml-1" />
             </Link>
           )}
         </div>
