85fbcb03a80465ef0e7ce251c91cc394f24203ac
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 20:01:43 +0300

    Create CustomDialog.tsx

---
+++ src/components/custom/CustomDialog.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+
+interface CustomDialogProps {
+  prop?: string;
+  children?: React.ReactNode;
+}
+
+const CustomDialog: FC<CustomDialogProps> = ({ children }) => {
+  return (
+    <Dialog>
+      <DialogTrigger className="uppercase font-bold w-full rounded-xl text-card-foreground mb-4">
+        View Full-Size Image
+      </DialogTrigger>
+      <DialogContent>
+        {children}
+        <DialogDescription className="text-center">
+          Images may appear smaller on mobile devices. For a clearer view,
+          consider checking on a larger screen.
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default CustomDialog;
