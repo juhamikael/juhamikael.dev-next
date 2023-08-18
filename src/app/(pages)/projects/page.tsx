86b10b84e79dbf594419782d01f89cedb0ec882d
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 19:59:31 +0300

    Update projects page

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -1,49 +1,17 @@
-import { FC } from "react";
-
 import ProjectCard from "@/components/custom/projects/ProjectCard";
-import fileOrganizerImg from "/public/projects/file-organizer.jpg";
-
-// MDX content
-import FileOrganizerText from "@/app/(content)/projects/fileOrganizer.mdx";
-import JuhaMikaelDev from "@/app/(content)/projects/juhamikael-dev.mdx";
-import SpotifyRecommendations from "@/app/(content)/projects/spotifyRecommendations.mdx";
-import SolitaDev from "@/app/(content)/projects/solitaDev-2023.mdx";
-import MacroCounter from "@/app/(content)/projects/macroCounter.mdx";
-import FLStudioStemRenamer from "@/app/(content)/projects/flStudioStemRenamer.mdx";
-import MakeNewFolder from "@/app/(content)/projects/makeNewFolder.mdx";
-import Unzipper from "@/app/(content)/projects/unzipper.mdx";
-import { projectQuery } from "@/../sanity/lib/queries";
-import { getSlugs } from "@/../sanity/lib/queries";
-import { cachedClient, client } from "@/../sanity/lib/client";
-
-//
-
+import { getProjectsData } from "@/lib/projects";
 export default async function ProjectsPage() {
-  const projects = await cachedClient(getSlugs);
-  // console.log(projects[0]);
+  const projectsData = await getProjectsData("projects-page-order");
 
-  const projectsDataPromises = projects[0].orderedProjects.map((project) => {
-    return cachedClient(projectQuery, {
-      slug: project.slug.current,
-      title: project.title,
-    });
-  });
-
-  const projectsData = await Promise.all(projectsDataPromises);
-  // console.log(projects[0].orderedProjects);
   return (
     <div className="flex flex-col gap-y-10">
-      {projectsData.map(async (project) => {
-        return (
-          <ProjectCard
-            key={project.slug}
-            project={await cachedClient(projectQuery, {
-              slug: project.slug.current,
-            })}
-            projectName={project.title}
-          />
-        );
-      })}
+      {projectsData.map((project) => (
+        <ProjectCard
+          key={project._id}
+          project={project}
+          projectName={project.title}
+        />
+      ))}
     </div>
   );
 }
