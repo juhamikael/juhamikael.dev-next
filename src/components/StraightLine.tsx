2741ac7b1eafc98574444baa52426e78235021fe
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 19:55:36 +0300

    Update StraightLine.tsx
    Add short documentation and modify the function a bit

--- src/components/StraightLine.tsx
+++ src/components/StraightLine.tsx
@@ -1,12 +1,23 @@
+import { cn } from "@/lib/utils";
 import React, { FC } from "react";
-interface IStarightLineProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const StraightLine: FC<IStarightLineProps> = ({ className }) => {
-  return (
-    <div
-      className={`border-b border-card-foreground/40 my-2 ${className}`}
-    ></div>
-  );
+interface IStarightLineProps extends React.HTMLAttributes<HTMLDivElement> {
+  opacity?: string;
+}
+/**
+ * Opacity of the line
+ * @default border-card-foreground/40
+ *
+ * @type {string}
+ *
+ * @example
+ * <StraightLine opacity="border-card-foreground/20" />
+ * <StraightLine opacity="border-blue-200/20" />
+ * Should be in tailwindcss format:
+ * border-<color>/<opacity>
+ */
+const StraightLine: FC<IStarightLineProps> = ({ className, opacity }) => {
+  const opacityValue = opacity ? opacity : "border-card-foreground/40";
+  return <div className={cn("border-b my-2", opacityValue, className)}></div>;
 };
 
 export default StraightLine;
