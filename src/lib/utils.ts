cb7107a975773e62507a4849c106ad94891813e9
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 19:53:47 +0300

    New Function to get image width and height

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getImageWidthAndHeight(url: string) {
+  if (!url) return { width: 300, height: 300 };
+  const regex = /-(\d+)x(\d+)\.jpg$/;
+  const match = url.match(regex);
+  if (match) {
+    const [_, width, height] = match;
+    return { width, height };
+  } else {
+    return { width: 300, height: 300 };
+  }
 }
