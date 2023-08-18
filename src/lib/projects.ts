8324cd774ee93fb153b483c0701a3b3ac4b59508
juhamikael <contact.juhamusic@gmail.com>
2023-08-18 19:54:41 +0300

    Utility function to get projects order
    Defining project order in sanity studio

---
+++ src/lib/projects.ts
@@ -0,0 +1,24 @@
+import { projectQuery } from "@/../sanity/lib/quories/projects/queries";
+import { getOrder } from "@/../sanity/lib/quories/projects/queries";
+import { cachedClient } from "@/../sanity/lib/client";
+
+interface Project {
+  slug: {
+    current: string;
+  };
+  title: string;
+}
+export const getProjectsData = async (slug: string) => {
+  const projects = await cachedClient(getOrder, { slug });
+  const projectsDataPromises =
+    projects && projects.orderedProjects
+      ? projects.orderedProjects.map((project: Project) => {
+          return cachedClient(projectQuery, {
+            slug: project.slug.current,
+            title: project.title,
+          });
+        })
+      : [];
+
+  return await Promise.all(projectsDataPromises);
+};
