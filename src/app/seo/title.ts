328704fe9d791b8f134aa52af3af9532aa8be548
juhamikael <contact.juhamusic@gmail.com>
2023-08-20 02:00:29 +0300

    Removing redundant files

--- src/app/seo/title.ts
+++
@@ -1,8 +0,0 @@
-export const title = {
-  home: "Juha Mikael / Home",
-  blog: "Juha Mikael / Blog",
-  projects: "Juha Mikael / Projects",
-  portfolio: "Juha Mikael / Portfolio",
-  skills: "Juha Mikael / Skills",
-  contact: "Juha Mikael / Contact",
-};
